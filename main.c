bfdbed690a6f1c9cb8d2fa16a5e72e2c9d0e18a7
Gaukhar Kambarbaeva <gkambarb@lab3r6s3.42abudhabi.ae>
2025-02-03 14:19:26 +0400

    interactive input

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <stdlib.h>
 
+int main(void)
+{
+	char *input;
+	
+	while (1)
+	{
+		input = readline("enter your input message\n");
+		if (!input)
+			break;
+		printf("You entered %s\n", input);
+		free(input);
+	}
+	return (0);
+}
 
+/*
 int main(int argc, char *argv[])
 {
 	if (argc == 2)
@@ -17,3 +33,4 @@ int main(int argc, char *argv[])
 		readline("input text string");
 	return (0);
 }
+*/
