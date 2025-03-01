f868e8ff59df0e1541b377d549d77f9760728f3c
root <root@DESKTOP-PJ6E4S8>
2025-03-01 17:54:49 +0400

    parser.c, v0

--- parser/parser.c
+++ parser/parser.c
@@ -1,18 +1,22 @@
-typedef struct
-{
-	char *command;
-	int count;
-} t_node;
+#include <../includes/parser.h>
+// #include "../minishell.h" //// Only if needed for global structures/macros
 
-void parser(char *str)
-{
-	char **cmd;
-	int i;
+// typedef struct
+// {
+// 	char *command;
+// 	int count;
+// } t_node;
+
+// void parser(char *str)
+// {
+// 	char **cmd;
+// 	int i;
 	
-	cmd = ft_split(str, ';');
-	i = 0;
-	while (cmd[i])
-	{
+// 	cmd = ft_split(str, ';');
+// 	i = 0;
+// 	while (cmd[i])
+// 	{
 
-	}
-}
+// 	}
+// }
+
