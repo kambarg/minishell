a5261c4c9a17f7ce7d0666a726834c81b0448a3e
kambarg <kambarg@gmail.com>
2025-07-10 10:56:45 +0400

    parser: norminette

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -42,48 +42,32 @@ static void	add_command(t_command **head, t_command *new_cmd)
 		current = current->next;
 	current->next = new_cmd;
 }
-// static int parse_tokens(t_token *tokens, t_command *current, t_command **commands)
-// {
-// 	while (tokens)
-// 	{
-// 		if (tokens->type == T_WORD)
-// 		{
-// 			if (!add_argument(current, tokens->value, tokens->quote_type))
-// 				return (0);
-// 		}
-// 		else if (tokens->type == T_PIPE)
-// 		{
-// 			add_command(commands, current);
-// 			current = create_command();
-// 		}
-// 		else if (tokens->type >= T_REDIR_IN && tokens->type <= T_APPEND)
-// 		{
-// 			if (!handle_redirect(&tokens, current))
-// 				return (0);
-// 		}
-// 		tokens = tokens->next;
-// 	}
-// 	return(1);
-// }
-// t_command	*parser(t_token *tokens)
-// {
-// 	t_command	*commands;
-// 	t_command	*current;
 
-// 	commands = NULL;
-// 	current = create_command();
-// 	if (!parse_tokens(tokens, current, &commands))
-// 	{
-// 		free(current->args);
-// 		free(current->redirects);
-// 		free(current);
-// 		if (commands)
-// 			free_commands(commands); // Assuming free_commands is defined to free the list
-// 		return (NULL);
-// 	}
-// 	add_command(&commands, current);
-// 	return (commands);
-// }
+static int	parse_tokens(t_token **tokens, t_command **cmd, t_command **cur)
+{
+	while (*tokens)
+	{
+		if ((*tokens)->type == T_WORD)
+		{
+			if (!add_argument(*cur, (*tokens)->value, (*tokens)->quote_type))
+				return (0);
+		}
+		else if ((*tokens)->type == T_PIPE)
+		{
+			add_command(cmd, *cur);
+			*cur = create_command();
+			if (!*cur)
+				return (0);
+		}
+		else if ((*tokens)->type >= T_REDIR_IN && (*tokens)->type <= T_APPEND)
+		{
+			if (!handle_redirect(tokens, *cur))
+				return (0);
+		}
+		*tokens = (*tokens)->next;
+	}
+	return (1);
+}
 
 t_command	*parser(t_token *tokens)
 {
@@ -92,25 +76,10 @@ t_command	*parser(t_token *tokens)
 
 	commands = NULL;
 	current = create_command();
-	while (tokens)
-	{
-		if (tokens->type == T_WORD)
-		{
-			if (!add_argument(current, tokens->value, tokens->quote_type))
-				return (NULL);
-		}
-		else if (tokens->type == T_PIPE)
-		{
-			add_command(&commands, current);
-			current = create_command();
-		}
-		else if (tokens->type >= T_REDIR_IN && tokens->type <= T_APPEND)
-		{
-			if (!handle_redirect(&tokens, current))
-				return (NULL);
-		}
-		tokens = tokens->next;
-	}
+	if (!current)
+		return (NULL);
+	if (!parse_tokens(&tokens, &commands, &current))
+		return (NULL);
 	add_command(&commands, current);
 	return (commands);
 }
