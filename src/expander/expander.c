6699430a5bb3f12f8be6b15e30dc847005595d79
kambarg <kambarg@gmail.com>
2025-07-14 23:14:06 +0400

    expander: expand quoted string is less than 25 lines

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -77,13 +77,38 @@ static char	*expand_var(char *str, t_shell *shell, int *i)
 		return (ft_strdup(""));
 }
 
+static char	*handle_dollar_char(char *str, t_shell *shell, int *i, char *result)
+{
+	char	*var;
+	char	*new_result;
+
+	var = expand_var(str, shell, i);
+	if (!var)
+		return (free(result), NULL);
+	new_result = ft_strjoin(result, var);
+	free(var);
+	if (!new_result)
+		return (free(result), NULL);
+	free(result);
+	return (new_result);
+}
+
+static char	*handle_regular_char(char *result, char c)
+{
+	char	*new_result;
+
+	new_result = ft_strjoin_char(result, c);
+	if (!new_result)
+		return (free(result), NULL);
+	free(result);
+	return (new_result);
+}
+
 // Note: single quotes no expansion at all
 static char	*expand_quoted_string(char *str, t_shell *shell, int quote_type)
 {
 	int		i;
 	char	*result;
-	char	*var;
-	char	*new_result;
 
 	if (!str || !shell)
 		return (NULL);
@@ -96,77 +121,16 @@ static char	*expand_quoted_string(char *str, t_shell *shell, int quote_type)
 	while (str[i])
 	{
 		if (str[i] == '$' && (quote_type == QUOTE_DOUBLE || quote_type == QUOTE_NONE))
-		{
-			var = expand_var(str, shell, &i);
-			if (!var)
-				var = ft_strdup("");
-			if (!var)
-			{
-				free(result);
-				return (NULL);
-			}
-			new_result = ft_strjoin(result, var);
-			free(var);
-			if (!new_result)
-			{
-				free(result);
-				return (NULL);
-			}
-			free(result);
-			result = new_result;
-		}
+			result = handle_dollar_char(str, shell, &i, result);
 		else
-		{
-			new_result = ft_strjoin_char(result, str[i]);
-			if (!new_result)
-			{
-				free(result);
-				return (NULL);
-			}
-			free(result);
-			result = new_result;
-			i++;
-		}
+			result = handle_regularchar(result, str[i++]);
+		if (!result)
+			return (NULL);
 	}
 	return (result);
 }
 
-static char	*safe_expand_string(char *str, t_shell *shell, int quote_type)
-{
-	char	*result;
-
-	result = expand_quoted_string(str, shell, quote_type);
-	if (result)
-		return (result);
-	
-	/* If expansion failed but we need to expand (not single quotes) */
-	if (quote_type != QUOTE_SINGLE)
-	{
-		/* Try to at least handle the most common cases: $? and $0 */
-		if (str && ft_strlen(str) == 2 && ft_strncmp(str, "$?", 2) == 0)
-		{
-			result = ft_itoa(shell->exit_status);
-			if (result)
-				return (result);
-		}
-		if (str && ft_strlen(str) == 2 && ft_strncmp(str, "$0", 2) == 0)
-		{
-			if (shell->program_name)
-				result = ft_strdup(shell->program_name);
-			else
-				result = ft_strdup("minishell");
-			if (result)
-				return (result);
-		}
-		/* If that fails too, return empty string as fallback */
-		return (ft_strdup(""));
-	}
-	
-	/* For single quotes, return original string */
-	return (ft_strdup(str));
-}
-
-// Redirections are always expanded
+// Note: redirections are always expanded
 void	expander(t_command *cmd, t_shell *shell)
 {
 	int			i;
@@ -180,7 +144,7 @@ void	expander(t_command *cmd, t_shell *shell)
 		i = 0;
 		while (i < cmd->arg_count)
 		{
-			expanded = safe_expand_string(cmd->args[i].value, shell, cmd->args[i].quote_type);
+			expanded = expand_quoted_string(cmd->args[i].value, shell, cmd->args[i].quote_type);
 			if (expanded)
 			{
 				free(cmd->args[i].value);
@@ -195,7 +159,7 @@ void	expander(t_command *cmd, t_shell *shell)
 		{
 			if (redir->type != REDIR_HEREDOC)
 			{
-				expanded = safe_expand_string(redir->file, shell, QUOTE_NONE);
+				expanded = expand_quoted_string(redir->file, shell, QUOTE_NONE);
 				if (expanded)
 				{
 					free(redir->file);
