cb82ff879aefe5d55418b3efff56b6ca7fd0f75f
WorodHazim <164627325+WorodHazim@users.noreply.github.com>
2025-07-16 15:11:46 +0400

    Update expand_var.c

--- src/expander/expand_var.c
+++ src/expander/expand_var.c
@@ -1,67 +1,70 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   expand_var.c                                       :+:      :+:    :+:   */
+/*   expander.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gkambarb <gkambarb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/15 14:10:06 by gkambarb          #+#    #+#             */
-/*   Updated: 2025/07/15 14:11:19 by gkambarb         ###   ########.fr       */
+/*   Created: 2025/07/15 14:09:56 by gkambarb          #+#    #+#             */
+/*   Updated: 2025/07/15 14:09:56 by gkambarb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static char	*get_var_name(char *str, int *i)
+static void	expand_args(t_command *cmd, t_shell *shell)
 {
-	int		start;
-	int		len;
-	char	*name;
+	int		i;
+	char	*expanded;
 
-	if (!str || !i)
-		return (NULL);
-	start = *i;
-	(*i)++;
-	if (str[*i] == '?')
-		return ((*i)++, ft_strdup("?"));
-	if (str[*i] == '0')
-		return ((*i)++, ft_strdup("0"));
-	while (str[*i] && (ft_isalnum(str[*i]) || str[*i] == '_'))
-		(*i)++;
-	len = *i - start - 1;
-	if (len == 0)
-		return (NULL);
-	name = (char *)malloc(sizeof(char) * (len + 1));
-	if (!name)
-		return (NULL);
-	ft_strlcpy(name, str + start + 1, len + 1);
-	return (name);
+	i = 0;
+	while (i < cmd->arg_count)
+	{
+		expanded = expand_quoted_string(cmd->args[i].value,
+				shell, cmd->args[i].quote_type);
+		if (expanded)
+		{
+			free(cmd->args[i].value);
+			cmd->args[i].value = expanded;
+		}
+		else
+			print_error("expander", "critical memory allocation failed");
+		i++;
+	}
 }
 
-char	*expand_var(char *str, t_shell *shell, int *i)
+// Note: redirections are always expanded
+static void	expand_redirects(t_command *cmd, t_shell *shell)
 {
-	char	*var_name;
-	char	*result;
+	t_redirect	*redir;
+	char		*expanded;
 
-	if (!str || !shell || !i)
-		return (ft_strdup("$"));
-	var_name = get_var_name(str, i);
-	if (!var_name)
-		return (ft_strdup("$"));
-	if (ft_strlen(var_name) == 1 && ft_strncmp(var_name, "?", 1) == 0)
-		result = ft_itoa(shell->exit_status);
-	else if (ft_strlen(var_name) == 1 && ft_strncmp(var_name, "0", 1) == 0)
+	redir = cmd->redirects;
+	while (redir)
 	{
-		if (shell->program_name)
-			result = ft_strdup(shell->program_name);
-		else
-			result = ft_strdup("minishell");
+		if (redir->type != REDIR_HEREDOC)
+		{
+			expanded = expand_quoted_string(redir->file, shell, QUOTE_NONE);
+			if (expanded)
+			{
+				free(redir->file);
+				redir->file = expanded;
+			}
+			else
+				print_error("expander", "critical memory allocation failed");
+		}
+		redir = redir->next;
 	}
-	else
-		result = get_env_value(shell->env, var_name);
-	free(var_name);
-	if (result)
-		return (result);
-	else
-		return (ft_strdup(""));
+}
+
+void	expander(t_command *cmd, t_shell *shell)
+{
+	if (!cmd || !shell)
+		return ;
+	while (cmd)
+	{
+		expand_args(cmd, shell);
+		expand_redirects(cmd, shell);
+		cmd = cmd->next;
+	}
 }
