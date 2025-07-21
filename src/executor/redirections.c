9a639b1a4666cd8719e812b06a1eb0f7d5f9dbdc
WorodHazim <164627325+WorodHazim@users.noreply.github.com>
2025-07-21 11:19:24 +0400

    Update redirections.c

--- src/executor/redirections.c
+++ src/executor/redirections.c
@@ -1,70 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirections.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkambarb <gkambarb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 14:54:43 by gkambarb          #+#    #+#             */
+/*   Updated: 2025/07/16 14:54:43 by gkambarb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/minishell.h"
 
-static int	create_heredoc_temp_file(char *delimiter, int *temp_fd, t_shell *shell)
-{
-	char	*line;
-	char	*temp_path;
-	int		write_fd;
-	int		read_fd;
-
-	/* Create temporary file path and write descriptor */
-	temp_path = create_unique_temp_path(shell);
-	if (!temp_path)
-	{
-		print_error("heredoc", "failed to create temp file");
-		return (ERROR);
-	}
-	
-	/* Open for writing */
-	write_fd = open(temp_path, O_WRONLY);
-	if (write_fd == -1)
-	{
-		print_error("heredoc", strerror(errno));
-		free(temp_path);
-		return (ERROR);
-	}
-	
-	setup_signals_heredoc();
-	
-	/* Write heredoc content to temporary file */
-	while (1)
-	{
-		line = readline("> ");
-		if (!line || ft_strncmp(line, delimiter, ft_strlen(delimiter) + 1) == 0)
-		{
-			free(line);
-			break ;
-		}
-		ft_putendl_fd(line, write_fd);
-		free(line);
-	}
-	
-	setup_signals_interactive();
-	
-	/* Close write descriptor */
-	close(write_fd);
-	
-	/* Open for reading before unlinking */
-	read_fd = open(temp_path, O_RDONLY);
-	if (read_fd == -1)
-	{
-		print_error("heredoc", strerror(errno));
-		unlink(temp_path);
-		free(temp_path);
-		return (ERROR);
-	}
-	
-	/* Now unlink to make file invisible and secure */
-	unlink(temp_path);
-	free(temp_path);
-	
-	/* Return read file descriptor */
-	*temp_fd = read_fd;
-	
-	return (SUCCESS);
-}
-
-/* Pre-process all heredocs in parent process before forking */
+// Pre-process all heredocs in parent process before forking:
+// 1) Create secure temp file and process heredoc in parent
+// 2) Store file descriptor in redirect structure
+// 3) Keep delimiter in file field for reference but mark as processed 
+// 4) Convert heredoc to special heredoc input redirect
 int	preprocess_heredocs(t_command *commands, t_shell *shell)
 {
 	t_command	*cmd;
@@ -79,15 +31,9 @@ int	preprocess_heredocs(t_command *commands, t_shell *shell)
 		{
 			if (redir->type == REDIR_HEREDOC)
 			{
-				/* Create secure temp file and process heredoc in parent */
-				if (create_heredoc_temp_file(redir->file, &temp_fd, shell) == ERROR)
+				if (create_temp_file(redir, &temp_fd, shell) == ERROR)
 					return (ERROR);
-				
-				/* Store file descriptor in redirect structure */
 				redir->fd = temp_fd;
-				
-				/* Keep delimiter in file field for reference but mark as processed */
-				/* Convert heredoc to special heredoc input redirect */
 				redir->type = REDIR_IN;
 			}
 			redir = redir->next;
@@ -97,11 +43,13 @@ int	preprocess_heredocs(t_command *commands, t_shell *shell)
 	return (SUCCESS);
 }
 
+// If temp_fd is provided (heredoc temp file), use it directly
+// Don't close temp_fd, will be closed when command structure is freed
+// Else regular file input redirect
 static int	handle_input_redirect(char *file, int temp_fd)
 {
 	int	fd;
 
-	/* If temp_fd is provided (heredoc temp file), use it directly */
 	if (temp_fd != -1)
 	{
 		if (dup2(temp_fd, STDIN_FILENO) == -1)
@@ -109,11 +57,8 @@ static int	handle_input_redirect(char *file, int temp_fd)
 			print_error("heredoc", strerror(errno));
 			return (ERROR);
 		}
-		/* Note: Don't close temp_fd here, it will be closed when command structure is freed */
 		return (SUCCESS);
 	}
-	
-	/* Regular file input redirect */
 	fd = open(file, O_RDONLY);
 	if (fd == -1)
 	{
@@ -156,6 +101,7 @@ static int	handle_output_redirect(char *file, int append)
 	return (SUCCESS);
 }
 
+// REDIR_HEREDOC should not happen anymore after preprocessing
 int	handle_redirections(t_redirect *redirects)
 {
 	while (redirects)
@@ -172,7 +118,6 @@ int	handle_redirections(t_redirect *redirects)
 		}
 		else if (redirects->type == REDIR_HEREDOC)
 		{
-			/* This should not happen anymore after preprocessing */
 			print_error("heredoc", "unexpected heredoc in child process");
 			return (ERROR);
 		}
@@ -184,4 +129,4 @@ int	handle_redirections(t_redirect *redirects)
 		redirects = redirects->next;
 	}
 	return (SUCCESS);
-} 
+}
