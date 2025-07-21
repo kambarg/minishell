f4cd1daf9ce00ec5cbc682fb82afa1711330b8cb
WorodHazim <164627325+WorodHazim@users.noreply.github.com>
2025-07-21 11:14:29 +0400

    Update echo.c

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -12,26 +12,40 @@
 
 #include "../../includes/minishell.h"
 
-int	ft_echo(t_arg_info *args, int arg_count)
+static int is_n_flag(const char *s)
 {
-	int	i;
-	int	n_flag;
+    int i;
 
-	n_flag = 0;
-	i = 1;
-	while (i < arg_count && ft_strncmp(args[i].value, "-n", 3) == 0)
-	{
-		n_flag = 1;
-		i++;
-	}
-	while (i < arg_count)
-	{
-		ft_putstr_fd(args[i].value, STDOUT_FILENO);
-		if (i + 1 < arg_count)
-			ft_putchar_fd(' ', STDOUT_FILENO);
-		i++;
-	}
-	if (!n_flag)
-		ft_putchar_fd('\n', STDOUT_FILENO);
-	return (SUCCESS);
+    if (!s || s[0] != '-' || s[1] != 'n')
+        return 0;
+    i = 2;
+    while (s[i])
+    {
+        if (s[i] != 'n')
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
+int ft_echo(t_arg_info *args, int arg_count)
+{
+    int i = 1;
+    int n_flag = 0;
+    while (i < arg_count && is_n_flag(args[i].value))
+    {
+        n_flag = 1;
+        i++;
+    }
+    while (i < arg_count)
+    {
+        ft_putstr_fd(args[i].value, STDOUT_FILENO);
+        if (i + 1 < arg_count)
+            ft_putchar_fd(' ', STDOUT_FILENO);
+        i++;
+    }
+    if (!n_flag)
+        ft_putchar_fd('\n', STDOUT_FILENO);
+
+    return SUCCESS;
 }
