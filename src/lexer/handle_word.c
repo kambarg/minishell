f54a862ecc2dac4cf28a5d3160ab5806e175ef1c
WorodHazim <164627325+WorodHazim@users.noreply.github.com>
2025-07-21 11:21:15 +0400

    Update handle_word.c

--- src/lexer/handle_word.c
+++ src/lexer/handle_word.c
@@ -3,14 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   handle_word.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gkambarb <gkambarb@student.42.fr>          +#+  +:+       +#+        */
+/*   By: worodhazem <worodhazem@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 11:17:46 by gkambarb          #+#    #+#             */
-/*   Updated: 2025/07/05 23:52:55 by gkambarb         ###   ########.fr       */
+/*   Updated: 2025/07/21 01:39:20 by worodhazem       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+static int	is_valid_identifier_char(char c)
+{
+	return (ft_isalnum(c) || c == '_');
+}
+
+static int	skip_quoted_value(char *input, int *i)
+{
+	char	quote;
+
+	if (!is_quotes(input[*i]))
+		return (0);
+	quote = input[*i];
+	(*i)++;
+	while (input[*i] && input[*i] != quote)
+		(*i)++;
+	if (input[*i] == quote)
+		(*i)++;
+	return (1);
+}
+
+static int	parse_assignment_value(char *input, int *i)
+{
+	if (is_quotes(input[*i]))
+	{
+		skip_quoted_value(input, i);
+	}
+	else
+	{
+		while (input[*i] && !is_whitespace(input[*i])
+			&& !is_operator_char(input[*i]))
+			(*i)++;
+	}
+	return (1);
+}
 
 static char	*get_word(char *input, int *i)
 {
@@ -21,7 +56,16 @@ static char	*get_word(char *input, int *i)
 	start = *i;
 	while (input[*i] && !is_whitespace(input[*i])
 		&& !is_operator_char(input[*i]) && !is_quotes(input[*i]))
+	{
+		if (input[*i] == '=' && *i > start
+			&& is_valid_identifier_char(input[*i - 1]))
+		{
+			(*i)++;
+			parse_assignment_value(input, i);
+			break ;
+		}
 		(*i)++;
+	}
 	len = *i - start;
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
