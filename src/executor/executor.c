78953ddf729d10f55919f6683dd65e95c51cd65f
WorodHazim <164627325+WorodHazim@users.noreply.github.com>
2025-07-21 11:16:31 +0400

    Update executor.c

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -1,40 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   executor.c                                         :+:      :+:    :+:   */
+/*   executor_args.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wuabdull <wuabdull@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/15 16:33:49 by wuabdull          #+#    #+#             */
-/*   Updated: 2025/07/16 15:06:39 by wuabdull         ###   ########.fr       */
+/*   Created: 2025/07/15 16:48:05 by wuabdull          #+#    #+#             */
+/*   Updated: 2025/07/15 16:48:15 by wuabdull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	is_builtin(char *cmd)
+char	**create_argv(t_command *cmd)
 {
-	return (ft_strncmp(cmd, "echo", 5) == 0
-		|| ft_strncmp(cmd, "cd", 3) == 0
-		|| ft_strncmp(cmd, "pwd", 4) == 0
-		|| ft_strncmp(cmd, "export", 7) == 0
-		|| ft_strncmp(cmd, "unset", 6) == 0
-		|| ft_strncmp(cmd, "env", 4) == 0
-		|| ft_strncmp(cmd, "exit", 5) == 0);
-}
+	char	**argv;
+	int		i;
 
-int	execute_commands(t_shell *shell)
-{
-	int		stdin_backup;
-	int		stdout_backup;
-	pid_t	last_pid;
-
-	if (preprocess_heredocs(shell->commands, shell) == ERROR)
-		return (ERROR);
-	stdin_backup = dup(STDIN_FILENO);
-	stdout_backup = dup(STDOUT_FILENO);
-	init_pipe_fds_loop(shell->commands);
-	setup_pipes_loop(shell->commands);
-	run_command_loop(shell, stdin_backup, stdout_backup, &last_pid);
-	return (wait_and_return_status(last_pid, stdin_backup, stdout_backup));
+	if (cmd->arg_count == 0)
+		return (NULL);
+	argv = (char **)malloc(sizeof(char *) * (cmd->arg_count + 1));
+	if (!argv)
+		return (NULL);
+	i = 0;
+	while (i < cmd->arg_count)
+	{
+		argv[i] = cmd->args[i].value;
+		i++;
+	}
+	argv[i] = NULL;
+	return (argv);
 }
