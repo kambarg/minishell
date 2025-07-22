fe35e299d0aeaf76cea9e1b5c17916a4a5d0318b
kambarg <kambarg@gmail.com>
2025-07-22 12:05:51 +0400

    Merge from Worod_curs branch echo.c source file

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -2,9 +2,12 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: wuabdull <wuabdull@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                    +:+ +:+        
+	+:+     */
+/*   By: wuabdull <wuabdull@student.42.fr>          +#+  +:+      
+	+#+        */
+/*                                                +#+#+#+#+#+  
+	+#+           */
 /*   Created: 2025/07/14 13:19:12 by wuabdull          #+#    #+#             */
 /*   Updated: 2025/07/14 13:19:12 by wuabdull         ###   ########.fr       */
 /*                                                                            */
@@ -12,14 +15,30 @@
 
 #include "../../includes/minishell.h"
 
+static int	is_n_flag(const char *s)
+{
+	int	i;
+
+	if (!s || s[0] != '-' || s[1] != 'n')
+		return (0);
+	i = 2;
+	while (s[i])
+	{
+		if (s[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	ft_echo(t_arg_info *args, int arg_count)
 {
 	int	i;
 	int	n_flag;
 
-	n_flag = 0;
 	i = 1;
-	while (i < arg_count && ft_strncmp(args[i].value, "-n", 3) == 0)
+	n_flag = 0;
+	while (i < arg_count && is_n_flag(args[i].value))
 	{
 		n_flag = 1;
 		i++;
