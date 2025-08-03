59782f87a0d5ed04066579298f41f6ef9df69f0d
kambarg <kambarg@gmail.com>
2025-08-03 18:06:56 +0400

    fixed err: before going to execute cmd check if the cmd is dir and return as cmd not found

--- src/executor/executor_external.c
+++ src/executor/executor_external.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executor_external.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gkambarb <gkambarb@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gkambarb <gkambarb@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 16:49:57 by wuabdull          #+#    #+#             */
-/*   Updated: 2025/07/28 18:27:52 by gkambarb         ###   ########.fr       */
+/*   Updated: 2025/08/03 18:02:02 by gkambarb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,15 @@ int	ft_strcmp(const char *s1, const char *s2)
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
+static int cmd_is_dir(char *cmd)
+{
+	struct stat cmd_stat;
+
+	ft_memset(&cmd_stat, 0, sizeof(cmd_stat));
+	stat(cmd, &cmd_stat);
+	return (S_ISDIR(cmd_stat.st_mode));
+}
+
 static void	exec_external_cmd(t_command *cmd, t_shell *shell)
 {
 	char	**argv;
@@ -29,21 +38,16 @@ static void	exec_external_cmd(t_command *cmd, t_shell *shell)
 
 	argv = create_argv(cmd);
 	cmd_path = find_command_path(cmd->args[0].value, shell->env);
-	if (!cmd_path)
+	if (!cmd_path || cmd_is_dir(cmd->args[0].value))
 	{
+		printf("DEBUG: cmd_path = %s\n", cmd_path);
 		print_error(cmd->args[0].value, "command not found");
 		free(argv);
 		exit(127);
 	}
 	reset_signals_default();
 	execve(cmd_path, argv, shell->env);
-	if (ft_strcmp(cmd->args[0].value, ".") == 0 || ft_strcmp(cmd->args[0].value,
-			"..") == 0)
-		print_error(cmd->args[0].value, "command not found");
-	else if (cmd->args[0].value[0] == '/')
-		print_error(cmd->args[0].value, "Is a directory");
-	else
-		print_error(cmd->args[0].value, strerror(errno));
+	print_error(cmd->args[0].value, strerror(errno));
 	free(cmd_path);
 	free(argv);
 	exit(126);
